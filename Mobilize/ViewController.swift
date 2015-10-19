78122148d2bd57ce9b1c9847116dbf874b481ed6
Miguel Araújo <miguelarauj1o@gmail.com>
2015-10-19 14:39:42 -0300

    from Xcode

---
+++ Mobilize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mobilize
+//
+//  Created by Miguel Araújo on 10/19/15.
+//  Copyright © 2015 Miguel Araújo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
