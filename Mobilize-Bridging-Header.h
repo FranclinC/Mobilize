29b2407ee0aabe7c9fdae737d664a061e8bf9354
Miguel Araújo <miguelarauj1o@gmail.com>
2015-11-11 17:40:49 -0300

    Merging master with fcmo by hand

---
+++ Mobilize-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Mobilize-Bridging-Header.h
+//  Mobilize
+//
+//  Created by Franclin Cabral on 20/10/15.
+//  Copyright © 2015 Miguel Araújo. All rights reserved.
+//
+
+#ifndef Mobilize_Bridging_Header_h
+#define Mobilize_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+
+#endif /* Mobilize_Bridging_Header_h */
