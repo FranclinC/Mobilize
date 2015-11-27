5105ff1775e207b52cf0aa9cdd49ca278c8cd4ca
franclinc <fcmo@cin.ufpe.br>
2015-11-27 15:35:23 -0300

    log out

--- Mobilize/AccountViewController.swift
+++ Mobilize/AccountViewController.swift
@@ -14,6 +14,12 @@ class AccountViewController: UIViewController, UITableViewDataSource, UITableVie
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //register nib
+        self.tableView.registerNib(UINib(nibName: "AccountCell", bundle: nil), forCellReuseIdentifier: "accountCell")
+        //register nib
+        self.tableView.registerNib(UINib(nibName: "AccountLogOut", bundle: nil), forCellReuseIdentifier: "accountLogOut")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +33,32 @@ class AccountViewController: UIViewController, UITableViewDataSource, UITableVie
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section
+        if section == 0 {
+            return 1
+        }else if section == 1 {
+            return 1
+        }else if section == 2 {
+            return 2
+        }else {
+            return 1
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //this could be better but I am too lazy to do it now
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("accountCell", forIndexPath: indexPath) as! ProposalDetailedCell
+            return cell
+        }else if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("accountCell", forIndexPath: indexPath) as! ProposalDetailedCell
+            return cell
+        }else if indexPath.row == 2 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("accountCell", forIndexPath: indexPath) as! ProposalDetailedCell
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("accountLogOut", forIndexPath: indexPath) as! ProposalDetailedCell
+            return cell
+        }
     }
     
     
