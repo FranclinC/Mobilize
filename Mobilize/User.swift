ad3764d9dc62ee8efa0dcc72814caff84eb20464
Miguel Araújo <miguelarauj1o@gmail.com>
2016-01-14 16:48:58 -0300

    removing unused files

--- Mobilize/User.swift
+++
@@ -1,9 +0,0 @@
-//
-//  User.swift
-//  Mobilize
-//
-//  Created by Miguel Araújo on 11/10/15.
-//  Copyright © 2015 Miguel Araújo. All rights reserved.
-//
-
-import Foundation
--- Mobilize/UserNotification.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UserNotification.swift
-//  Mobilize
-//
-//  Created by Miguel Araújo on 11/11/15.
-//  Copyright © 2015 Miguel Araújo. All rights reserved.
-//
-
-import Foundation
